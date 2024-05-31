ecfaa1ab83b7a4df56394211a6f773c206c4b87e
Bruno <brunoslopes.dev@gmail.com>
2024-05-30 22:44:56 -0300

    feat: add details page

---
+++ src/app/details/details.page.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { DetailsPage } from './details.page';
+
+describe('DetailsPage', () => {
+  let component: DetailsPage;
+  let fixture: ComponentFixture<DetailsPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DetailsPage],
+      imports: [IonicModule.forRoot(), ExploreContainerComponentModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DetailsPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
